78adfa1843e6b862c215fea4d82d88df4e28bbe1
dlrjsgml44 <162662402+dlrjsgml44@users.noreply.github.com>
2024-04-13 19:16:19 +0900

    [Silver II] Title: 스택 수열, Time: 212 ms, Memory: 42248 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+t = int(input())
+a = list(map(int, [input() for i in range(t)])) #입력 받기
+res = deque([0]) #스택에 저장하기
+z = 1 #스택에 추가되는 값
+q = deque()# pop 한거 저장
+ok = False # "NO" 인지 확인
+for i in a:
+    if ok:
+        break
+    else:
+        while True:
+            if res[-1] == i:
+                q.append('-')
+                res.pop()
+                break
+            if z> i*2:
+                ok = True
+                break
+            res.append(z)
+            z+=1
+            q.append('+')
+
+if ok:
+    print("NO")
+else:
+    for i in q:
+        print(i)
