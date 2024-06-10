0858cbe8fe8ddfc59947be0f0f3ac61f802d376a
이건희 <162662402+tumblecat44@users.noreply.github.com>
2024-06-10 15:49:43 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+T = int(sys.stdin.readline().strip())
+c = deque()
+for i in range(T):
+    a =  sys.stdin.readline().split()
+    if a[0] == "push":
+        c.append(a[1])
+    elif a[0] == "pop":
+        if c:
+            print(c.popleft())
+        else:
+            print(-1)
+    elif a[0]== "size":
+        print(len(c))
+    elif a[0] == "empty":
+        if c:
+            print(0)
+        else:
+            print(1)
+    elif a[0]=="front":
+        if c:
+            print(c[0])
+        else:
+            print(-1)
+    elif a[0]=="back":
+        if c:
+            print(c[-1])
+        else:
+            print(-1)
