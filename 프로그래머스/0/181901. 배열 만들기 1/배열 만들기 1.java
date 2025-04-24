ecfafc1e2d75755aeba015df59acaf84e0a82dfe
이건희 <162662402+tumblecat44@users.noreply.github.com>
2025-04-24 16:53:27 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.10 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[(n/k)];
+        int count = 0;
+        for(int i= k;  i<=n; i+=k){
+            answer[count++] = i;
+        }
+        return answer;
+    }
+}
